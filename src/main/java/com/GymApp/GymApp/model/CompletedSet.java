009b03b7e982ae2fdad8ab6876e46bb6953172f8
Alberth Martin <alberth.martin01@gmail.com>
2025-10-02 19:14:41 +0300

    Started the implementation of workout History. Enitites CompletedSet and CompletedWorkout

---
+++ src/main/java/com/GymApp/GymApp/model/CompletedSet.java
@@ -0,0 +1,29 @@
+package com.GymApp.GymApp.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class CompletedSet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int reps;
+    private double weight;
+
+    @ManyToOne
+    @JoinColumn(name = "workout_id", nullable = false)
+    private CompletedWorkout workout;
+
+    @ManyToOne
+    @JoinColumn(name = "exercise_id", nullable = false)
+    private Exercise exercise;
+}
---
+++ src/main/java/com/GymApp/GymApp/model/CompletedWorkout.java
@@ -0,0 +1,46 @@
+package com.GymApp.GymApp.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Time;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class CompletedWorkout {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    private LocalDateTime completedAt;
+
+    private Time duration;
+    private String notes;
+
+    @OneToMany(mappedBy = "workout", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CompletedSet> sets = new ArrayList<>();
+
+    public void addSet(CompletedSet set) {
+        sets.add(set);
+        set.setWorkout(this);
+    }
+
+    public void removeSet(CompletedSet set) {
+        sets.remove(set);
+        set.setWorkout(null);
+    }
+}
